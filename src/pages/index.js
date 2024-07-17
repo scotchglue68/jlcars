174519cd585f05452179269434183e80dcbdab51
scotchglue68 <bethlizyxu@gmail.com>
2024-07-16 20:10:44 -0400

    fix bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
-import Service from './Service'
 import Vehicle from './Vehicle'
 import Customer from './Customer'
 import MenuAppBar from './MenuAppBar'
 
 export {
-    Service,
     Vehicle,
     Customer,
     MenuAppBar
